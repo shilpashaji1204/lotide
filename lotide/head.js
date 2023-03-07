e7f1b9af55400952e6de05c2461c2868a81434c6
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-07 04:34:41 -0500

    Array in assertion

---
+++ lotide/head.js
@@ -0,0 +1,16 @@
+function head(arr)
+{
+    return arr[0]
+}
+const assertEqual = function(actual, expected)
+{
+if(actual === expected)
+{
+    console.log("Passed");
+}
+else{
+    console.log("failed");
+}
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
