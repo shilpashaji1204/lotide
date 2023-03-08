4ec9239fe48d53af975f9799d192968f5545a63d
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-08 09:07:48 -0500

    Assertequal arrays

---
+++ lotide/assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function(arr1, arr2)
+{
+    if(arr1.length !== arr2.length)
+    {
+        return false;
+    }
+    for (let i=0; i < arr1.length; i++)
+    {
+        if(arr1[i] !== arr2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+const assertArraysEqual = function(arr1, arr2)
+{
+    const result = eqArrays(arr1,arr2);
+    if(!result)
+    {
+        console.log(`🛑 Arrays are not equal`);
+    }
+    else{
+        console.log(`✅ Arrays are equal`);
+    }
+}
+assertArraysEqual([1, 2, 3], [1, 2]);
