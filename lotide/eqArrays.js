0304299dfb3f877ebb9a1a689d45c4694b316037
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-08 08:39:42 -0500

    Compare two arrays

---
+++ lotide/eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log("✅ assertion Passed");
+    } else if (actual !== expected) {
+      console.log("🛑 assertion failed");
+    }
+  };
+
+
+const eqArrays = function(arr1, arr2)
+{
+    if(arr1.length !== arr2.length)
+    {
+        return false;
+    }
+    for (let i=0; i < arr1.length; i++)
+    {
+        if(arr1[i] !== arr2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
