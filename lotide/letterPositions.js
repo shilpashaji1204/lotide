535a1e521e48049e3f48fde77375cb6f4a1e1c3e
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-09 06:14:09 -0500

    Implement letterPositions

---
+++ lotide/letterPositions.js
@@ -0,0 +1,50 @@
+const eqArrays = function(arr1, arr2)
+{
+    if(arr1.length !== arr2.length)
+    {
+        return false;
+    }
+    for (let i=0; i < arr1.length; i++)
+    {
+        if(arr1[i] !== arr2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+const assertArraysEqual = function(arr1, arr2)
+{
+    const result = eqArrays(arr1,arr2);
+    if(!result)
+    {
+        console.log(`🛑 Arrays are not equal`);
+    }
+    else{
+        console.log(`✅ Arrays are equal`);
+    }
+}
+const letterPositions = function(sentence)
+{
+    const result = {};
+    
+    for(let i = 0; i < sentence.length; i++)
+    {
+        const letters = sentence[i];
+        if(letters !== '')
+        {
+        if(!result[letters])
+        {
+        result[letters] = [i];
+        }
+        else{
+            result[letters].push(i);
+        }   
+    }
+}
+    return result;
+};
+const result = letterPositions("hello");
+console.log(result);
+
+assertArraysEqual(letterPositions("hello").e, [1]);
