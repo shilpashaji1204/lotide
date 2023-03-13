0bc2a5aa67ea6e853449a3c6870a506817abb76c
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-13 18:16:46 -0400

    modified head.js

--- lotide/head.js
+++ lotide/head.js
@@ -14,3 +14,5 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
 
 module.exports = head;
+
+
--- lotide/test/headTest.js
+++ lotide/test/headTest.js
@@ -1,17 +1,14 @@
 
-const head = require('../head');
-const assert = require('chai').assert;
-function test(head) {
-describe("#head", () => {
-    it("returns 1 for [1,2,3]", () =>{
-      assert.strictEqual(head([1,2,3]), 1);    
-    });
-    
-    it("returns '5' for ['5']", () => {
-      assert.strictEqual(head(['5']), '5'); 
-    });
-  });
-}
-//assertEqual(head([5,6,7]), 5);
-//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
 
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
